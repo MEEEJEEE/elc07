0aab6dfcdd06d5be3ed9b6ff07da47f88b9363df
miji <mijilee0413@sookmyung.ac.kr>
2023-10-19 15:56:46 +0900

    실습4

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//실습4
+
+void f(void);
+
+int i;
+int main(void)
+{
+	for (i=0; i<5; i++)
+	{
+		f();
+	}
+	return 0;
+}
+
+void f(void)
+{
+	for (i=0; i<10; i++)
+	   printf("#");
+}
